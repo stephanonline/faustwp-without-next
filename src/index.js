ce707737e0c4040319b996f2b60ccca430d59867
Stephan Oomen <s.oomen@mplus.nl>
2023-06-21 16:03:04 +0200

    Add faustwp/core withouth next

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setConfig } from '@faustwp/core/dist/cjs/config/index'
+import Page from './templates/Page'
+
+export class OverrideGraphqlEndpointPlugin {
+  apply(hooks) {
+    const { addFilter } = hooks
+
+    addFilter(
+      'graphqlEndpoint',
+      'reactonly',
+      // eslint-disable-next-line no-unused-vars
+      (graphqlEndpoint, context) => {
+        console.log(process.env.REACT_APP_WORDPRESS_URL)
+        return `${process.env.REACT_APP_WORDPRESS_URL}/graphql`
+      }
+    )
+  }
+}
+
+setConfig({
+  templates: {
+    page: Page,
+    single: Page,
+    index: Page,
+  },
+  experimentalPlugins: [new OverrideGraphqlEndpointPlugin()]
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/templates/Page.js
@@ -0,0 +1,31 @@
+import { gql } from "@apollo/client";
+
+export default function Component(props) {
+  if (props.loading) {
+    return <>Loading...</>
+  }
+  // Code to render Single Post template hierachy type
+  return <>{props.data.page.title}</>;
+}
+
+// eslint-disable-next-line no-undef
+Component.query = gql`
+  query GetPageData($databaseId: ID!) {
+    page(id: $databaseId, idType: DATABASE_ID) {
+      title
+      seo {
+        title
+        metaDesc
+        fullHead
+      }
+    }
+  }
+`;
+
+Component.variables = (query, ctx) => {
+  console.log(query)
+  const { databaseId } = query;
+  return {
+    databaseId: 2,
+  };
+};
